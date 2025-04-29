906277820b740e5ea6bb728ce1e3436a189ce3b2
Anirudh Vyas <av@goldilocksfashion>
2025-04-29 07:20:38 -0700

    latest xaeroflux updates

--- src/zero_proof.rs
+++ src/zero_proof.rs
@@ -1,4 +1,5 @@
 use rkyv::{Archive, Deserialize, Serialize};
+use xaeroflux::core::XaeroData;
 
 #[repr(C)]
 #[derive(Debug, Clone, Archive, Serialize, Deserialize)]
@@ -8,3 +9,8 @@ pub struct ZeroProof {
     pub journal: Vec<u8>,
     pub seal: Vec<u32>,
 }
+
+pub trait ZeroProofMarker {}
+
+impl ZeroProofMarker for ZeroProof {}
+
--- src/zero_wallet_manager.rs
+++ src/zero_wallet_manager.rs
@@ -1,42 +1,9 @@
-use std::collections::HashMap;
+use crate::{nft::Nft, zero_id::ZeroId, zero_proof::ZeroProof, zero_wallet::MAX_NFTS};
 
-use crate::{
-    nft::Nft,
-    zero_id::ZeroId,
-    zero_proof::ZeroProof,
-    zero_wallet::MAX_NFTS,
-};
 pub trait ZeroWalletManager {
-    fn new() -> Self;
+    fn zero_id(&self) -> ZeroId;
+    fn proofs(&self) -> Vec<ZeroProof>;
+    fn nfts(&self) -> Vec<Nft>;
     fn add_proofs(&mut self, proofs: Vec<ZeroProof>);
-    fn add_proof(&mut self, proof: ZeroProof);
-    fn remove_proof(&mut self, proof_id: &str);
-    fn add_nft(&mut self, nft: Option<Nft>);
-    fn remove_nft(&mut self, nft_id: &str);
-    fn get_proof(&self, proof_id: &str) -> Option<&ZeroProof>;
-    fn get_nft(&self, nft_id: &str) -> Option<&Nft>;
-    fn list_proofs(&self) -> Vec<&ZeroProof>;
-    fn list_nfts(&self) -> Vec<&Nft>;
-    fn get_id(&self) -> &ZeroId;
-    fn set_id(&mut self, id: ZeroId);
-    fn get_proofs(&self) -> &HashMap<String, ZeroProof>;
-    fn get_nfts(&self) -> &[Option<Nft>; MAX_NFTS];
-    fn set_proofs(&mut self, proofs: HashMap<String, ZeroProof>);
-    fn set_nfts(&mut self, nfts: [Option<Nft>; MAX_NFTS]);
-    fn clear_proofs(&mut self);
-    fn clear_nfts(&mut self);
-    fn clear(&mut self);
-    fn is_empty(&self) -> bool;
-    fn is_full(&self) -> bool;
-    fn is_valid(&self) -> bool;
-    fn is_valid_proof(&self, proof: &ZeroProof) -> bool;
-    fn is_valid_nft(&self, nft: &Nft) -> bool;
-    fn is_valid_id(&self, id: &ZeroId) -> bool;
-    fn is_valid_proofs(&self, proofs: &HashMap<String, ZeroProof>) -> bool;
-    fn is_valid_nfts(&self, nfts: &[Option<Nft>; MAX_NFTS]) -> bool;
-    fn is_valid_wallet(&self) -> bool;
-    fn is_valid_wallet_id(&self) -> bool;
-    fn is_valid_wallet_proofs(&self) -> bool;
-    fn is_valid_wallet_nfts(&self) -> bool;
-    fn is_valid_wallet_proof(&self, proof: &ZeroProof) -> bool;
+    fn add_nfts(&mut self, nfts: Vec<Nft>);
 }
